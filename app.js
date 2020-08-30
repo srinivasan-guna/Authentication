b7307e012c86c9eeb8122ababa471e510df5367b
srinivasan-guna <srinivasan-guna@github.com>
2020-08-30 12:54:12 +0530

    Facebook and Google login update

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://secretsweb.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -90,7 +90,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: "http://localhost:3000/auth/facebook/secrets",
+        callbackURL: "https://secretsweb.herokuapp.com/auth/facebook/secrets",
         profileFields: ["id", "email"]
     },
     function (accessToken, refreshToken, profile, cb) {
